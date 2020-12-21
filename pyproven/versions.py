6adf373426c00dcceb6053e8947835b6683465e2
Cremily <crilly1@gmail.com>
2020-12-21 16:08:02 +1100

    Rename of version data classes
    Now matches javascript naming.

--- pyproven/versions.py
+++ pyproven/versions.py
@@ -4,26 +4,33 @@ from typing import Any, Dict, Optional, Union,List
 
 from datetime import datetime
 
-class VersionData(UserDict):
-    def __init__(self,document:Dict[str,Any]):
-        super().__init__(document)
-        self.response: str = document['response']
-        self.version: float = document['version']
-        self.status: str = document['status']
-class GetVersionData(VersionData):
+class CompactResponse(UserDict):
+    #TODO
     pass
-class SetVersionData(VersionData):
-    pass
-class ListVersionsSingularData(UserDict):
+
+
+class ListVersionDocument(UserDict):
     def __init__(self,document:Dict[str,any]):
         super().__init__(document)
         self.version = document['version']
         self.status = document['status']
         self.effective_date = document['effectiveDate']
-class ListVersionsData(UserDict):
-    def __init__(self,versions:List[Dict[str,Any]]):
-        self.versions = [ListVersionsSingularData(doc) for doc in versions]
+class ListVersionsResponse(UserDict):
+    def __init__(self,document:Dict[str,Any]):
+        self.ok = document['ok']
+        self.versions = [ListVersionDocument(doc) for doc in document['versions']]
         super().__init__({'versions':self.versions})
         
-        
+
+class VersionResponse(UserDict):
+    def __init__(self,document:Dict[str,Any]):
+        super().__init__(document)
+        self.response: str = document['response']
+        self.version: float = document['version']
+        self.status: str = document['status']
+class GetVersionResponse(VersionResponse):
+    pass
+class SetVersionResponse(VersionResponse):
+    pass
+
         
