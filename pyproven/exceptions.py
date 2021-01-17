3e9258215bceadd9dd7589a5ac65930ce27b3ee9
Cremily <crilly1@gmail.com>
2021-01-17 14:24:38 +1100

    Edited docstrings.

--- pyproven/exceptions.py
+++ pyproven/exceptions.py
@@ -95,4 +95,4 @@ class GetVersionProofException(PyProvenException):
     """Exception raised when :class:`pyproven.database.ProvenDB` fails to get the proof document for a specific version. """
 
 class ListStorageException(ListStorageException):
-    """Exception raised when :class:`pyproven.database.ProvenDB`fails to get the list of storage sizes for each collection in the db. 
+    """Exception raised when :class:`pyproven.database.ProvenDB`fails to get the list of storage sizes for each collection in the db."""
